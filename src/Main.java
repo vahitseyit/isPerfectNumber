a02131fbba4ab661fdfed37250a625283384b194
Vahit Seyit Basa <vahitbasa@marun.edu.tr>
2023-11-26 12:03:47 +0300

    Numbers can be entered from the user. Fixed logic error in loop

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int number = 49;
-        int positiveDivisor = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number you want to test if it is the perfect number");
+        int number = input.nextInt();
+        int positiveDivisorSum = 1;//The sum of positive divisors starts from 1 because 1 is the positive divisor of all numbers.
         boolean isPerfect = false;
-        for (int i = 1; i < number; i++) {
-            if (number % i == 0) positiveDivisor += i;
-            if (number / positiveDivisor == 1) isPerfect = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) positiveDivisorSum += i;//Find the positive divisors of the entered number
+            if (number == positiveDivisorSum) isPerfect = true;//
         }
         if (!isPerfect) System.out.println("Number is not a perfect number");
-        else System.out.println("Number is  a perfect number");
+        else System.out.println("Number is a perfect number");
     }
 }
